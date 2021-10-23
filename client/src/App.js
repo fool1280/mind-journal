00490c9ead2f2fe4381806b701fc736003bf7025
Chika Dueke-Eze <chika.duekeeze@duke.edu>
2021-10-23 19:17:48 -0400

    added home and navbar component // commented out main class

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,22 @@
 import './styles/App.css';
+import Navbar from "./Navbar";
+import Home from './Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className='container'>
-      <main className='main'>
-        <h1 className='title'>
-          Welcome to <a href='#'>Mind Journal!</a>
-        </h1>
-      </main>
-    </div>
+
+    <Router>
+      <div className='container'>
+      <Navbar/>
+      <Home/>
+        {/* <main className='main'>
+          <h1 className='title'>
+            Welcome to <a href='#'>Mind Journal!</a>
+          </h1>
+        </main> */}
+      </div>
+    </Router>
   );
 }
 
