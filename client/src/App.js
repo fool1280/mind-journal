9af8c9abd9e791be244ed3f15f39efc265a1adcd
tthuwng <tronghung199@gmail.com>
2021-10-23 16:48:43 -0500

    move back to react js

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import './styles/App.css';
+
+function App() {
+  return (
+    <div className='container'>
+      <main className='main'>
+        <h1 className='title'>
+          Welcome to <a href='#'>Mind Journal!</a>
+        </h1>
+      </main>
+    </div>
+  );
+}
+
+export default App;
