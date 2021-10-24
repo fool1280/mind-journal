03667f6ecaad7e5989c2dea6a665b9511ccbef57
Chika Dueke-Eze <chika.duekeeze@duke.edu>
2021-10-23 21:48:02 -0400

    implemented navbar

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -1,37 +1,37 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import Scroll from 'react-scroll'
+const ScrollLink = Scroll.ScrollLink
+
 
 const Navbar = () => {
     const [navbar, setNavbar] = useState(false);
 
 
-    // const changeBackground = ()=>{
-    //     if(window.scrollY >= 75){
-    //         setNavbar(true)
-    //     }else{
-    //         setNavbar(false);
-    //     }
-    // }
+    const changeBackground = ()=>{
+        if(window.scrollY >= 75){
+            setNavbar(true)
+        }else{
+            setNavbar(false);
+        }
+    }
 
-    // window.addEventListener("scroll", changeBackground)
-
+    window.addEventListener("scroll", changeBackground)
 
     return ( 
         <nav className={navbar ? "navbar active" : "navbar"}>
             <div className="logo">
                 <Link to="/">
-                    Mind-Journal
+                M-J 
                 </Link>
             </div>
             <ul className="nav-dropdwon">
+                
                 <li>
-                    <Link to="/">About</Link>
+                    <Link to="/#how-iw">How it works</Link>
                 </li>
                 <li>
-                    <Link to="/gallery">How it works</Link>
-                </li>
-                <li>
-                    <Link to="/wallet">Features</Link>
+                    <Link to="/features">Features</Link>
                 </li>
                 <li>FAQ</li>
             </ul>
