c0f5151d484c5a0af79153446137d807d08c3f66
Chika Dueke-Eze <chika.duekeeze@duke.edu>
2021-10-23 21:47:49 -0400

    fixed classnames // added high-level "todo" at bottom

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,24 +1,49 @@
+import { Element } from 'react-scroll'
 
 const Home = () => {
 
     return ( 
-        <section className="home">
-            <div class="hero-section">
-                <div class="hero-1">
-                    <h1 class="hero-title">
-                        Capture your thoughts/emotions frictionlessly!<span className="dot">.</span>
-                    </h1>
-                    <p class="hero-paragraph">
-                        Mind-Journal is a comprehensive tool that empowers people to practice deliberate mindfulness and boost vibes by sending users good memories regularly.
-                    </p>
-                    <div className="sign-in">Claim a Plot</div>
+        <div className="container">
+            {/*modal section*/}
+            <section className="home">
+                <div class="hero-section">
+                    <div class="hero-1">
+                        <h1 class="hero-title">
+                            Capture your thoughts/emotions frictionlessly!<span className="dot">.</span>
+                        </h1>
+                        <p class="hero-paragraph">
+                            M-J is a comprehensive tool that empowers people to practice deliberate mindfulness and boost vibes by sending users good memories regularly.
+                        </p>
+                        <p>okay, cool but why should i care about recording my thoughts? here's why</p>
+                        {/* <div className="sign-in">Claim a Plot</div> */}
 
+                    </div>
+                    <div class="hero-illustration">
+                        <img src={"/illustration-1.png"} alt="ddd"/>
+                    </div>
                 </div>
-                <div class="hero-illustration">
-                    <img src={"/eth-logo-black.png"} alt="ddd"/>
-                </div>
-            </div>
-        </section>
+            </section>
+
+            {/* add "how-it-works" section*/}
+            {/* add "features" section*/}
+            {/* add FAQ section */}
+
+
+
+
+
+            {/*---------- tried doing something but it didn't work *----------/}
+            
+            {/* <Element id='how-iw' name='example-destination'>
+            // wrap your content in the Element from react-scroll
+            <section id="how-iw">
+
+                <div style={{border: "1px solid black"}}></div>
+
+
+            </section> 
+            </Element> */}
+        </div>
      );
 }
  
